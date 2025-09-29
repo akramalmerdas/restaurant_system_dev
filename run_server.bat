ceebc40f35e1ede11c5b231501f719e8c337707c
AkramAlmerdas <akramalmerdas@gmail.com>
2025-09-29 19:21:58 +0200

    Fix .gitignore: Untrack all compiled files and cache.

--- run_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-
-:: Start Daphne server in a new terminal window
-start "Daphne Server" cmd /k " daphne -b 0.0.0.0 -p 8000 MochaCafe.asgi:application"
-
-:: Wait for 5 seconds (adjust if needed)
-
